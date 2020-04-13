3f95751ab90baaa8b029bf2d42fed8b77625163b
Abhinav  Gupta <gabhi8740@gmail.com>
2020-04-13 23:15:43 +0530

    first commit

---
+++ src/main/java/com/example/Radha/RadhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Radha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Radha/RadhaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Radha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RadhaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
